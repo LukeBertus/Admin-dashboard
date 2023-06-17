caed0f5bfaf52294227a3efc913d51da6d3266b5
LukeBertus <vboxuser@Xubuntu2.myguest.virtualbox.org>
2023-06-17 14:02:55 +0800

    Implement responsize search icon colors

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const newButton = document.querySelector(".new")
 const uploadButton = document.querySelector(".upload")
 const shareButton = document.querySelector(".share")
-const notiPath = document.querySelector(".notiPath")
+
 const notiSvg = document.querySelector(".notifications")
+
+const searchSvg = document.querySelector(".searchIcon")
 
 newButton.addEventListener('click', e => window.location.href = "file:///home/vboxuser/repos/admin-dashboard/index.html")
 uploadButton.addEventListener('click', e => window.location.href = "file:///home/vboxuser/repos/admin-dashboard/index.html")
@@ -10,3 +12,6 @@ shareButton.addEventListener('click', e => window.location.href = "file:///home/
 
 notiSvg.addEventListener('mouseenter', e => notiSvg.setAttribute("fill", "#f55449"));
 notiSvg.addEventListener('mouseleave', e => notiSvg.setAttribute("fill", "#000000"));
+
+searchSvg.addEventListener('mouseenter', e => searchSvg.setAttribute("fill", "#f55449"));
+searchSvg.addEventListener('mouseleave', e => searchSvg.setAttribute("fill", "#000000"));
