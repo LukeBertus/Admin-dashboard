2c5ac2e9d556ce2fa08fa76bd50a095b912f78c3
LukeBertus <vboxuser@Xubuntu2.myguest.virtualbox.org>
2023-06-17 13:53:18 +0800

    Implement responsive notification svg

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const newButton = document.querySelector(".new")
 const uploadButton = document.querySelector(".upload")
 const shareButton = document.querySelector(".share")
+const notiPath = document.querySelector(".notiPath")
+const notiSvg = document.querySelector(".notifications")
 
 newButton.addEventListener('click', e => window.location.href = "file:///home/vboxuser/repos/admin-dashboard/index.html")
 uploadButton.addEventListener('click', e => window.location.href = "file:///home/vboxuser/repos/admin-dashboard/index.html")
 shareButton.addEventListener('click', e => window.location.href = "file:///home/vboxuser/repos/admin-dashboard/index.html")
+
+notiSvg.addEventListener('mouseenter', e => notiSvg.setAttribute("fill", "#f55449"));
+notiSvg.addEventListener('mouseleave', e => notiSvg.setAttribute("fill", "#000000"));
