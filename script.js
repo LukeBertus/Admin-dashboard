ee9b74376bfe5cdda10fd1f8f3725f9decf6c7c5
LukeBertus <vboxuser@Xubuntu2.myguest.virtualbox.org>
2023-06-17 13:26:39 +0800

    Add hero layout

---
+++ script.js
@@ -0,0 +1,7 @@
+const newButton = document.querySelector(".new")
+const uploadButton = document.querySelector(".upload")
+const shareButton = document.querySelector(".share")
+
+newButton.addEventListener('click', e => window.location.href = "file:///home/vboxuser/repos/admin-dashboard/index.html")
+uploadButton.addEventListener('click', e => window.location.href = "file:///home/vboxuser/repos/admin-dashboard/index.html")
+shareButton.addEventListener('click', e => window.location.href = "file:///home/vboxuser/repos/admin-dashboard/index.html")
